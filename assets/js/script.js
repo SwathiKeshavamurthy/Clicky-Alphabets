f9f4db920f6824c930b10c59f911e4f3c18fe260
swathi keshavamurthy <swathik.2197@gmail.com>
2024-01-19 22:29:33 +0000

    Add event listeners to buttons

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+/**
+ * Add event listeners to Start and Reset buttons on DOM load
+ * Functions to start and reset the game will be attached
+ */
+document.addEventListener('DOMContentLoaded', function () {
+   const buttons = document.getElementsByTagName('button');
+
+});
