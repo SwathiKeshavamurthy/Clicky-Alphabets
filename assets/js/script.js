e2f09d834af3517c17f25d8308c00006354880b2
swathi keshavamurthy <swathik.2197@gmail.com>
2024-01-19 22:49:45 +0000

    Call function handleClick to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,21 @@
 /**
  * Add event listeners to Start and Reset buttons on DOM load
- * Functions to start and reset the game will be attached
  */
 document.addEventListener('DOMContentLoaded', function () {
    const buttons = document.getElementsByTagName('button');
 
-});
+    //Add eventlisteners to Start and Reset buttons.
+    for (let button of buttons) {
+        button.addEventListener('click', handleClick);
+    }
+    //Functions to start and reset Buttons
+    function handleClick(event) {
+        if (event.target.getAttribute('data-type') === 'start') {
+            alert('You hit the Start button');
+        } else if (event.target.getAttribute('data-type') === 'reset') {
+            alert('You hit the reset button');
+        } else {
+            alert('No game is running, please press "Start"');
+        }
+    }
+    });
