5944a25e887c0af0c250433b6a921f9d1afd3d91
swathi keshavamurthy <swathik.2197@gmail.com>
2024-01-21 00:00:50 +0000

    Add besttime function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,12 @@ document.addEventListener('DOMContentLoaded', function () {
    let secondsInterval; //Keep track of timer count
    let second = 0; //Set displaying time on page
    let timerRunning = false; //Game stopped
-   let firstAlphabet = A; //Keep count of what alphabet is clicked
-
-
+   let firstNumber = 1; //Keep count of what alphabet is clicked
 
     //Add eventlisteners to Start and Reset buttons.
     for (let button of buttons) {
         button.addEventListener('click', handleClick);
     }
-
 
     //Functions to start and reset Buttons
     function handleClick(event) {
@@ -22,6 +19,7 @@ document.addEventListener('DOMContentLoaded', function () {
             secondsInterval = setInterval(setTimer, 1000); //Start timer
             timerRunning = true; //Start game
             document.getElementById('start-button').disabled = true;
+
         } else if (event.target.getAttribute('data-type') === 'reset') {
             clearInterval(secondsInterval);
             second = 0;
@@ -32,7 +30,7 @@ document.addEventListener('DOMContentLoaded', function () {
             document.getElementById('start-button').disabled = false;
 
             resetDivs();
-            addNumToDiv();
+            addAlphabetToDiv();
 
         } else {
             alert('No game is running, please press "Start"');
@@ -52,14 +50,15 @@ document.addEventListener('DOMContentLoaded', function () {
     function handleDivClick() {
         if (timerRunning === true) {
             let divAlphabet = parseInt(this.textContent);//Get Alphabet from clicked div
-            if (divAlphabet === firstAlphabet && firstAlphabet < Z) {
+            if (divAlphabet === firstNumber && firstNumber < 26) {
                 this.style.backgroundColor = 'green';//If Alphabet is correct, color background green
-                firstAlphabet++;
+                firstNumber++;
 
-        } else if (divAlphabet !== firstAlphabet) { 
-            /*setTimeout function
-            Waits 0.1 sec before displaying alert message.
-            Had to use this so div would color red before alert displays.*/
+        } else if (divAlphabet !== firstNumber) { 
+            /**setTimeout function
+            *Waits 0.1 sec before displaying alert message.
+            *Had to use this so div would color red before alert displays.
+            */
 
             setTimeout(function () {
                 alert('Game Over! Press Reset and then Start, to start a new game.');
@@ -67,11 +66,10 @@ document.addEventListener('DOMContentLoaded', function () {
             this.style.backgroundColor = 'red';
             timerRunning = false;
             clearInterval(secondsInterval);//Stop timer
-            
-        } else { 
-            //Game complete functionality
 
-            if (firstAlphabet=== Z) {
+          //Game complete functionality  
+        } else {
+            if (firstNumber === 26) {
                 setTimeout(function () {
                     alert('Congratulations, you completed the game!');
                 }, 100);
@@ -100,15 +98,15 @@ addAlphabetToDiv();
 
 //Creates an array and fills it randomly with Alphabets A-Z
 function randomArray() {
-    const array=[]; //Create an empty array
+    const array = []; //Create an empty array
 
     while (array.length < 26) {
-        let alphabet = Math.floor(Math.random() * 26) + 1; //Random alphabets A-Z
+        let number = Math.floor(Math.random() * 26) + 1; //Random numbers 1-26
 
-        if (array.includes(alphabet)) {
-            continue; //If alpahbet already exists in array, skip this iteration
+        if (array.includes(number)) {
+            continue; //If number already exists in array, skip this iteration
         } else {
-            array.push(alphabet); //Push unique alphabet into array
+            array.push(number); //Push unique alphabet into array
         }
     }
     return array;
@@ -122,10 +120,10 @@ function randomArray() {
 */
 function addAlphabetToDiv() {
     let divs = document.getElementsByClassName('alphabet-div');
-    let alphabets = randomArray(); //Function that generats array with random, unique Alphabets A-Z
+    let numbers = randomArray(); //Function that generats array with random, unique Alphabets A-Z
 
     for (let i = 0; i < divs.length; i++) {
-        divs[i].textContent = alphabets[i]; //Add the alphabets from randomArray to each alphabet-div in the html
+        divs[i].textContent = numbers[i]; //Add the alphabets from randomArray to each alphabet-div in the html
     }
 }
 
@@ -138,5 +136,22 @@ function resetDivs() {
 
     for (let div of divs) {
         div.style.backgroundColor = 'rgb(219, 197, 248);';
+    }
+}
+
+/**
+ * Get the time on timer when game is completed.
+ * Add it to Your best time if time is shorter, or if it is 0.
+ **/
+
+function bestTime() {
+    let time = document.getElementById('timer').textContent;
+    let newTime = parseInt(time.slice(12)); //Extract the number of seconds from timer
+    let bestTime = document.getElementById('best-time');
+    let bestTimeValue = parseInt(bestTime.textContent.slice(26)); //Extract number from best time
+
+    //Compare extracted numbers and update Your best time
+    if (newTime < bestTimeValue || bestTimeValue === 0) {
+        bestTime.textContent = `Your best time: ${newTime} seconds`;
     }
 }
